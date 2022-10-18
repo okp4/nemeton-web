ecb8dc022e84a71909b17cd00e24df89e1795f5b
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-18 14:19:29 +0200

    chore(config): disable css modules in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-}
+const path = require('path')
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  webpack(config) {
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } }
+    config.module.rules[2].oneOf?.forEach(one => {
+      if (!`${one.issuer?.and}`.includes('_app')) return
+      one.issuer.and = [path.resolve()]
+    })
+    return config
+  }
+}
