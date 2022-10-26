e403bd593acfe21181437215f395c44b3be7abb3
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-26 21:37:14 +0200

    chore(config): add env files and types + version in next config file

---
+++ environment.d.ts
@@ -0,0 +1,25 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    OKP4_DISCORD_URL: string
+    OKP4_GITHUB_URL: string
+    OKP4_LINKEDIN_URL: string
+    OKP4_MEDIUM_URL: string
+    OKP4_TELEGRAM_URL: string
+    OKP4_TWITTER_URL: string
+    OKP4_WEBSITE_URL: string
+    OKP4_DOCS_URL: string
+    OKP4_DOCS_NODES_URL: string
+    OKP4_DOCS_FAQ_URL: string
+    OKP4_TYPEFORM_URL: string
+  }
+}
+
+declare module 'next/config' {
+  declare const _default: () => {
+    publicRuntimeConfig: {
+      version: string
+    }
+  }
+
+  export default _default
+}
--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const pkg = require('./package.json')
 
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  publicRuntimeConfig: {
+    version: pkg.version
+  },
   webpack(config) {
     config.experiments = { ...config.experiments, ...{ topLevelAwait: true } }
     config.module.rules[2].oneOf?.forEach(one => {
