f6b5cd4ca386ad5ca18826f85d479bba5e3d7a1b
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-26 21:39:04 +0200

    feat: add a type folder in src for shared types

---
+++ src/types/config.type.ts
@@ -0,0 +1,19 @@
+export type Config = {
+  app: {
+    websiteUrl: string
+    socialMediaUrls: {
+      discordUrl: string
+      githubUrl: string
+      linkedinUrl: string
+      mediumUrl: string
+      telegramUrl: string
+      twitterUrl: string
+    }
+    docsUrls: {
+      whitepaperUrl: string
+      nodesUrl: string
+      faqUrl: string
+    }
+    typeformUrl: string
+  }
+}
