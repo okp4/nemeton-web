0f210a21bc5cd819e16c6f5c30c9994a3a640586
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-26 22:28:53 +0200

    feat(ui): handle social media urls in footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,31 +1,79 @@
 import Image from 'next/image'
+import getConfig from 'next/config'
+import React, { useMemo } from 'react'
 import { useMediaType } from '../../../hook/useMediaType'
+import type { Config } from '../../../types/config.type'
 import './footer.scss'
 
-const okp4Links: string[] = ['discord', 'github', 'linkedin', 'medium', 'telegram', 'twitter']
+type SocialMedia = {
+  label: string
+  url: string
+}
 
-export const Footer = (): JSX.Element => {
-  const isLargeScreen = useMediaType('(min-width: 1441px)')
-  const isMobileScreen = useMediaType('(max-width: 390px)')
+type FooterProps = {
+  staticUrls: Config['app']
+}
+
+export const Footer: React.FC<FooterProps> = ({ staticUrls }): JSX.Element => {
+  const { socialMediaUrls, websiteUrl } = staticUrls
+  const socialMedias: SocialMedia[] = useMemo(
+    () => [
+      {
+        label: 'discord',
+        url: socialMediaUrls.discordUrl
+      },
+      {
+        label: 'github',
+        url: socialMediaUrls.githubUrl
+      },
+      {
+        label: 'linkedin',
+        url: socialMediaUrls.linkedinUrl
+      },
+      {
+        label: 'medium',
+        url: socialMediaUrls.mediumUrl
+      },
+      {
+        label: 'telegram',
+        url: socialMediaUrls.telegramUrl
+      },
+      {
+        label: 'twitter',
+        url: socialMediaUrls.twitterUrl
+      }
+    ],
+    [socialMediaUrls]
+  )
+  const isLargeScreen = useMediaType('(min-width: 1540px)')
+  const isMobileScreen = useMediaType('(max-width: 580px)')
   const iconSize = isLargeScreen || isMobileScreen ? 50 : 38
+  const { publicRuntimeConfig } = getConfig()
   return (
     <div className="okp4-nemeton-web-footer-container">
       <div className="okp4-nemeton-web-footer-content">
-        {isLargeScreen ? (
-          <Image
-            alt="okp4-inline-logo"
-            height={110}
-            src="/image/okp4-inline-logo.png"
-            width={396}
-          />
-        ) : (
-          <Image
-            alt="okp4-inline-logo"
-            height={isMobileScreen ? 62 : 85}
-            src="/image/okp4-inline-logo.png"
-            width={isMobileScreen ? 225 : 305}
-          />
-        )}
+        <a
+          href={websiteUrl}
+          rel="noreferrer"
+          target="_blank"
+          title={`v${publicRuntimeConfig.version}`}
+        >
+          {isLargeScreen ? (
+            <Image
+              alt="okp4-inline-logo"
+              height={110}
+              src="/image/okp4-inline-logo.png"
+              width={396}
+            />
+          ) : (
+            <Image
+              alt="okp4-inline-logo"
+              height={isMobileScreen ? 62 : 85}
+              src="/image/okp4-inline-logo.png"
+              width={isMobileScreen ? 225 : 305}
+            />
+          )}
+        </a>
         <div className="okp4-nemeton-web-footer-links-container">
           <p>Support</p>
           <div className="okp4-nemeton-web-footer-links-divider" />
@@ -34,14 +82,16 @@ export const Footer = (): JSX.Element => {
           <p>Whitepaper</p>
         </div>
         <div className="okp4-nemeton-web-footer-socials-media-container">
-          {okp4Links.map((link: string) => (
-            <Image
-              alt={`${link}-link`}
-              height={iconSize}
-              key={link}
-              src={`/icons/${link}-round-dark.svg`}
-              width={iconSize}
-            />
+          {socialMedias.map((socialMedia: SocialMedia) => (
+            <a href={socialMedia.url} key={socialMedia.url} rel="noreferrer" target="_blank">
+              <Image
+                alt={`${socialMedia.label}-link`}
+                className="okp4-nemeton-web-footer-socials-media-icon"
+                height={iconSize}
+                src={`/icons/${socialMedia.label}-round-dark.svg`}
+                width={iconSize}
+              />
+            </a>
           ))}
         </div>
       </div>
