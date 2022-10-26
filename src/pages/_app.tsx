43a800be84fc1af468c41d01514b2f09ff60d06e
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-26 21:38:20 +0200

    feat(ui): fetch config from api and pass as props to pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../main.scss'
 import type { AppProps } from 'next/app'
+import type { Config } from '../types/config.type'
+import { useEffect, useState } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+const fetchConfig = async (): Promise<Config> => {
+  const response = await fetch('/api/config')
+  return await response.json()
+}
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element | null {
+  const [config, setConfig] = useState<Config | null>(null)
+
+  useEffect(() => {
+    fetchConfig()
+      .then(setConfig)
+      .catch((error: unknown) => console.error(error))
+  }, [])
+  return config && <Component {...pageProps} staticUrls={config.app} />
 }
 
 export default MyApp
