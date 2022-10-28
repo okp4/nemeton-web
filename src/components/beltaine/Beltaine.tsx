8de2b8caf329cb2100264220db8d37d66a968657
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-28 10:57:49 +0200

    refactor(ui): generify phases and own style

--- src/components/beltaine/Beltaine.tsx
+++ src/components/beltaine/Beltaine.tsx
@@ -1,23 +1,23 @@
-import './beltaine.scss'
+const description =
+  'The third phase is all about token dynamics! Druids will engage in various node and community tasks with their precious tokens. Challenges will include some IBC-related tasks to open Nemeton to the interchain world...ime. Maintenance tasks and upgrades will be performed to test different kinds of state migrations.'
 
 export const Beltaine = (): JSX.Element => {
-  const description =
-    'The third phase is all about token dynamics! Druids will engage in various node and community tasks with their precious tokens. Challenges will include some IBC-related tasks to open Nemeton to the interchain world...ime. Maintenance tasks and upgrades will be performed to test different kinds of state migrations.'
   return (
-    <div className="okp4-nemeton-web-beltaine-container">
-      <div className="okp4-nemeton-web-beltaine-phase">
-        <div className="okp4-nemeton-web-beltaine-announcement">
-          <div className="okp4-nemeton-web-beltaine-divider" />
-          <h2>Coming Soon</h2>
-          <div className="okp4-nemeton-web-beltaine-divider" />
-        </div>
-        <div className="okp4-nemeton-web-beltaine-phase-details">
-          <div className="okp4-nemeton-web-beltaine-phase-title">
-            <h2>Phase 3</h2>
-            <h1>Beltaine</h1>
+    <div className="okp4-nemeton-web-phase-main ">
+      <div className="okp4-nemeton-web-phase-container beltaine">
+        <div className="okp4-nemeton-web-phase-content-container no-border">
+          <div className="okp4-nemeton-web-phase-mask-container">
+            <div className="okp4-nemeton-web-phase-mask-divider" />
+            <h2>Coming Soon</h2>
+            <div className="okp4-nemeton-web-phase-mask-divider" />
           </div>
-          <p>{description}</p>
-          <button>Challenges & Rewards</button>
+          <div className="okp4-nemeton-web-sidh-phase-content-details">
+            <div className="okp4-nemeton-web-sidh-phase-content-title">
+              <h2>Phase 3</h2>
+              <h1>Beltaine</h1>
+            </div>
+            <p>{description}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/imbolc/Imbolc.tsx
+++ src/components/imbolc/Imbolc.tsx
@@ -1,21 +1,23 @@
-import './imbolc.scss'
+const description =
+  "The second phase is focused on testing Druids' performance and uptime. Maintenance tasks and upgrades will be performed to test different kinds of state migrations."
 
 export const Imbolc = (): JSX.Element => {
-  const description =
-    "The second phase is focused on testing Druids' performance and uptime. Maintenance tasks and upgrades will be performed to test different kinds of state migrations."
   return (
-    <div className="okp4-nemeton-web-imbolc-container">
-      <div className="okp4-nemeton-web-imbolc-phase">
-        <div className="okp4-nemeton-web-imbolc-announcement">
-          <h2>OPEN January 4th @10A.M.</h2>
-        </div>
-        <div className="okp4-nemeton-web-imbolc-phase-details">
-          <div className="okp4-nemeton-web-imbolc-phase-title">
-            <h2>Phase 2</h2>
-            <h1>Imbolc</h1>
+    <div className="okp4-nemeton-web-phase-main ">
+      <div className="okp4-nemeton-web-phase-container imbolc">
+        <div className="okp4-nemeton-web-phase-content-container no-border">
+          <div className="okp4-nemeton-web-phase-mask-container">
+            <div className="okp4-nemeton-web-phase-mask-divider" />
+            <h2>Coming Soon</h2>
+            <div className="okp4-nemeton-web-phase-mask-divider" />
           </div>
-          <p>{description}</p>
-          <button>Challenges & Rewards</button>
+          <div className="okp4-nemeton-web-sidh-phase-content-details">
+            <div className="okp4-nemeton-web-sidh-phase-content-title">
+              <h2>Phase 2</h2>
+              <h1>Imbolc</h1>
+            </div>
+            <p>{description}</p>
+          </div>
         </div>
       </div>
     </div>
