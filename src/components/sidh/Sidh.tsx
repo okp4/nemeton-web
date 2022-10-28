61f7b843f2ef686af5d0b97e6ffa5dc63cd6882e
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-10-28 10:58:46 +0200

    refactor(ui): enhance and generify Sidh component

--- src/components/sidh/Sidh.tsx
+++ src/components/sidh/Sidh.tsx
@@ -1,32 +1,50 @@
-import { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { SidhChallenges } from './Challenges'
-import './sidh.scss'
+import { useMediaType } from '../../hook/useMediaType'
 
 export const Sidh = (): JSX.Element => {
   const [isChallengesOpen, setIsChallengesOpen] = useState<boolean>(false)
+  const challengeContainerRef = useRef<HTMLDivElement | null>(null)
+  const isMobileScreen = useMediaType('(max-width: 580px)')
   const description =
     " This first phase is pretty basic, it is dedicated to setting up Druids' validator environment, participating in the genesis, and getting familiar with the OKP4 testnet."
 
-  const toggleChallengesOpen = useCallback(
-    () => setIsChallengesOpen(!isChallengesOpen),
-    [isChallengesOpen]
-  )
+  const toggleChallengesOpen = useCallback(() => {
+    setIsChallengesOpen(!isChallengesOpen)
+  }, [isChallengesOpen])
+
+  useEffect(() => {
+    if (isChallengesOpen) {
+      challengeContainerRef.current?.scrollIntoView({ behavior: 'smooth', block: 'nearest' })
+    }
+  }, [isChallengesOpen])
 
   return (
-    <div className="okp4-nemeton-web-sidh-with-challenges-container">
-      <div className="okp4-nemeton-web-sidh-container">
-        <div className="okp4-nemeton-web-sidh-phase">
-          <div className="okp4-nemeton-web-sidh-phase-details">
-            <div className="okp4-nemeton-web-sidh-phase-title">
+    <div className="okp4-nemeton-web-phase-main ">
+      <div className="okp4-nemeton-web-phase-container sidh">
+        <div className="okp4-nemeton-web-phase-content-container">
+          <div className="okp4-nemeton-web-sidh-phase-content-details">
+            <div className="okp4-nemeton-web-sidh-phase-content-title">
               <h2>Phase 1</h2>
               <h1>Sidh</h1>
             </div>
             <p>{description}</p>
-            <button onClick={toggleChallengesOpen}>Challenges & Rewards</button>
+            {!isMobileScreen && (
+              <span className="okp4-nemeton-web-challenge-button" onClick={toggleChallengesOpen}>
+                Challenges & Rewards
+              </span>
+            )}
           </div>
         </div>
       </div>
-      {isChallengesOpen && <SidhChallenges />}
+      {isMobileScreen && (
+        <span className="okp4-nemeton-web-challenge-button" onClick={toggleChallengesOpen}>
+          Challenges & Rewards
+        </span>
+      )}
+      {isChallengesOpen && (
+        <SidhChallenges onClose={toggleChallengesOpen} refObj={challengeContainerRef} />
+      )}
     </div>
   )
 }
