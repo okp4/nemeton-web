09dfcb01ed563ef6a298eb670cd53a3a3501603c
ErikssonJoakim <j.eriksson@okp4.com>
2022-10-31 13:48:41 +0100

    refactor: move ul out of p & remove redundant line break

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -21,37 +21,34 @@ const Support: NextPage<SupportProps> = ({ keywords, title, urls }) => {
         <Header typeformUrl={typeformUrl} />
         <div className="okp4-nemeton-web-page-content-container">
           <h1>Support page</h1>
-          <p>
-            For any help please consult these links for answers:
+          <p>For any help please consult these links for answers:</p>
+          <ul>
+            <li>
+              <a href={nodesUrl} rel="noreferrer" target="_blank">
+                Nodes & Validators Guide
+              </a>
+            </li>
             <br />
-            <ul>
-              <li>
-                <a href={nodesUrl} rel="noreferrer" target="_blank">
-                  Nodes & Validators Guide
-                </a>
-              </li>
-              <br />
-              <li>
-                <Link href="/faq">FAQ</Link>
-              </li>
-              <br />
-              <li>
-                OKP4 Discord server:
-                <ul>
-                  <li>
-                    <a href={discordNemetonUrl} rel="noreferrer" target="_blank">
-                      Nemeton program
-                    </a>
-                  </li>
-                  <li>
-                    <a href={discordTicketUrl} rel="noreferrer" target="_blank">
-                      Request ticket
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </p>
+            <li>
+              <Link href="/faq">FAQ</Link>
+            </li>
+            <br />
+            <li>
+              OKP4 Discord server:
+              <ul>
+                <li>
+                  <a href={discordNemetonUrl} rel="noreferrer" target="_blank">
+                    Nemeton program
+                  </a>
+                </li>
+                <li>
+                  <a href={discordTicketUrl} rel="noreferrer" target="_blank">
+                    Request ticket
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
         <Footer urls={urls} />
       </main>
