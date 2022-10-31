a5c81679e5779568fe9c842cf1b6a8fe0551a92e
ErikssonJoakim <j.eriksson@okp4.com>
2022-10-31 11:15:04 +0100

    feat: remove line break between paragraphs in rewards page

--- src/pages/rewards.tsx
+++ src/pages/rewards.tsx
@@ -4,21 +4,19 @@ import { Footer } from '../components/layout/footer/Footer'
 import { Header } from '../components/layout/header/Header'
 import type { Config } from '../types/config.type'
 
-export type RewardsProps = Pick<Config, "title" | "keywords" | "urls">
-
+export type RewardsProps = Pick<Config, 'title' | 'keywords' | 'urls'>
 
 const Rewards: NextPage<RewardsProps> = ({ keywords, title, urls }) => {
   const { typeformUrl } = urls
 
   return (
     <div className="okp4-nemeton-web-page-main">
-      <Head keywords={keywords} title={title}/>
+      <Head keywords={keywords} title={title} />
       <main>
         <Header typeformUrl={typeformUrl} />
         <div className="okp4-nemeton-web-page-content-container">
           <h1>Rewards</h1>
           <p>Druids will be rewarded with a mix of token rewards + delegation commitment.</p>
-          <br />
           <p>
             1% of the total supply of mainnet tokens will be given to Druids as a reward based on
             their participation and performance during the program.Many more tokens (OKP4 foundation
