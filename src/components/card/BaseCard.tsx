af0d2b914ca75b43f8c72797cf9d9071000bbf31
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-11-17 15:06:04 +0100

    feat(ui): add styled basic card

---
+++ src/components/card/BaseCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type BaseCardProps = {
+  title?: JSX.Element
+  description?: JSX.Element
+  backgroundImageUrl?: string
+}
+
+export const BaseCard: React.FC<BaseCardProps> = ({ title, description, backgroundImageUrl }) => (
+  <div className="okp4-nemeton-web-base-card-main">
+    {title && <div className="okp4-nemeton-web-base-card-title-container">{title}</div>}
+    {description && (
+      <div className="okp4-nemeton-web-base-card-description-container">{description}</div>
+    )}
+  </div>
+)
