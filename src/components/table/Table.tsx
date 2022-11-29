81bf7691d966f9f5e120da73959f7094020cb719
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-11-29 22:10:08 +0100

    feat(ui): enhance table component

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,99 +1,31 @@
+import { Skeleton } from '@mui/material'
 import classNames from 'classnames'
 import Image from 'next/image'
+import hatDruidAnimationData from '../../../public/json/hat-druid.json'
 import React, { useCallback, useMemo, useState } from 'react'
+import { useMediaType } from '../../hook/useMediaType'
+import type { Druid } from '../../entity/druid'
 import { Search } from '../search/Search'
 import { Snackbar } from '../snackbar/Snackbar'
+import { LottieLoader } from '../loader/LottieLoader'
 
 export type Column = {
   label: string
-  renderCell: (data: Data) => JSX.Element
+  renderCell: (druid: Druid) => JSX.Element
   width?: string
+  hidden?: boolean
 }
 
-export type Data = {
-  rank: number
-  identity: {
-    avatar: string
-    name: string
-  }
-  address: string
-  tasks: {
-    completed: number
-    total: number
-  }
-  points: number
+export type TableProps = {
+  data: Druid[]
+  onSearchChange: (value: string) => void
+  loading: boolean
+  loadingMore?: boolean
 }
 
-const data: Data[] = [
-  {
-    rank: 1,
-    identity: {
-      avatar: '/image/avatar-fallback.webp',
-      name: 'christophe camel'
-    },
-    address: 'okp4valoper12484t0zw9ztqvepm6pw774p6k4ye4jqr2q0nue',
-    tasks: {
-      completed: 3,
-      total: 3
-    },
-    points: 8907
-  },
-  {
-    rank: 2,
-    identity: {
-      avatar: '/image/avatar-fallback.webp',
-      name: 'fred vilcot'
-    },
-    address: 'okp4valoper13484t0zw9ztqvepb6pw884p6k4ya4jqr2q0pue',
-    tasks: {
-      completed: 2,
-      total: 3
-    },
-    points: 7347
-  },
-  {
-    rank: 3,
-    identity: {
-      avatar: '/image/beltaine-medium.webp',
-      name: 'arnaud mimart'
-    },
-    address: 'okp4valoper1cgyf7qy8pdrwaj803lywwul0l6ykpdthashzd2',
-    tasks: {
-      completed: 2,
-      total: 3
-    },
-    points: 5821
-  },
-  {
-    rank: 4,
-    identity: {
-      avatar: '/image/sidh-medium.webp',
-      name: 'homer simpson'
-    },
-    address: 'okp4valoper1dxv5dlg4vvmux00h0v7vy8d3mflju93yq4hhfh',
-    tasks: {
-      completed: 1,
-      total: 3
-    },
-    points: 4377
-  },
-  {
-    rank: 5,
-    identity: {
-      avatar: '/image/avatar-fallback.webp',
-      name: 'jean bonbeurre'
-    },
-    address: 'okp4valoper1etx55kw7tkmnjqz0k0mups4ewxlr324tdwqwss',
-    tasks: {
-      completed: 1,
-      total: 3
-    },
-    points: 2090
-  }
-]
-
-export const Table: React.FC = () => {
+export const Table: React.FC<TableProps> = ({ data, onSearchChange, loading, loadingMore }) => {
   const [address, setAddress] = useState<string>('')
+  const isMobileScreen = useMediaType('(max-width: 580px)')
 
   const handleCopyAddress = useCallback(
     (address: string) => () => {
@@ -104,65 +36,108 @@ export const Table: React.FC = () => {
   )
 
   const columns: Column[] = useMemo(
-    () => [
-      {
-        label: 'Rank',
-        renderCell: (data: Data) => <span>{data.rank.toLocaleString()}</span>,
-        width: '9%'
-      },
-      {
-        label: 'Name',
-        renderCell: (data: Data) => (
-          <div className="flex-cell">
-            <Image
-              alt="validator-avatar"
-              className="avatar-bg"
-              height={21}
-              src={data.identity.avatar}
-              width={21}
-            />
-            <span>{data.identity.name}</span>
-          </div>
-        ),
-        width: '29%'
-      },
-      {
-        label: 'Address',
-        width: '25%',
-        renderCell: (data: Data) => (
-          <div className="flex-cell">
-            <span>{data.address}</span>
-            <span
-              className="okp4-nemeton-web-table-copy-logo"
-              onClick={handleCopyAddress(data.address)}
-              title="Copy on clipboard"
-            />
-          </div>
-        )
-      },
-      {
-        label: 'Completed tasks',
-        width: '22%',
-        renderCell: (data: Data) => (
-          <span>{`${data.tasks.completed.toString()}/${data.tasks.total.toString()}`}</span>
-        )
-      },
-      {
-        label: 'Total points',
-        width: '15%',
-        renderCell: (data: Data) => <span>{data.points.toLocaleString()}</span>
-      }
-    ],
-    [handleCopyAddress]
+    () =>
+      [
+        {
+          label: 'Rank',
+          renderCell: (druid: Druid) => <span>{druid.rank.toLocaleString()}</span>,
+          width: isMobileScreen ? '18%' : '9%'
+        },
+        {
+          label: 'Name',
+          renderCell: (druid: Druid) => (
+            <div className="flex-cell">
+              <Image
+                alt="validator-avatar"
+                className="avatar-bg"
+                height={21}
+                src={druid.identity.avatar}
+                width={21}
+              />
+              <span>{druid.identity.name}</span>
+            </div>
+          ),
+          width: isMobileScreen ? '56%' : '29%'
+        },
+        {
+          label: 'Address',
+          width: '25%',
+          renderCell: (druid: Druid) => (
+            <div className="flex-cell">
+              <span>{druid.valoper}</span>
+              <span
+                className="okp4-nemeton-web-table-copy-logo"
+                onClick={handleCopyAddress(druid.valoper)}
+                title="Copy on clipboard"
+              />
+            </div>
+          ),
+          hidden: isMobileScreen
+        },
+        {
+          label: 'Completed tasks',
+          width: '22%',
+          renderCell: (druid: Druid) => (
+            <span>{`${druid.tasks.completed.toString()}/${druid.tasks.finished.toString()}`}</span>
+          ),
+          hidden: isMobileScreen
+        },
+        {
+          label: 'Total points',
+          width: isMobileScreen ? '26%' : '15%',
+          renderCell: (druid: Druid) => <span>{druid.points.toLocaleString()}</span>
+        }
+      ].filter(column => !column.hidden),
+    [handleCopyAddress, isMobileScreen]
   )
 
-  const handleSearchChange = useCallback((value: string) => {
-    console.log(value)
-  }, [])
+  const handleSearchChange = useCallback(
+    (value: string) => {
+      onSearchChange(value)
+    },
+    [onSearchChange]
+  )
 
   const handleSnackbarClose = useCallback(() => {
     setAddress('')
   }, [setAddress])
+
+  const loadingRows = useMemo(
+    () =>
+      [...Array(4)].map((_elt, index) => (
+        <tr key={index}>
+          {columns.map((_column, index) => (
+            <td key={index}>
+              <Skeleton
+                height="35px"
+                sx={{ bgcolor: 'rgba(255, 255, 255, 0.13)', borderRadius: '10px' }}
+                variant="rectangular"
+                width="80%"
+              />
+            </td>
+          ))}
+        </tr>
+      )),
+    [columns]
+  )
+
+  const fetchMoreLoader = useMemo(
+    () => (
+      <>
+        <tr>
+          <td colSpan={5} style={{ textAlign: 'center', paddingTop: '20px' }}>
+            <LottieLoader animationData={hatDruidAnimationData} width={isMobileScreen ? 80 : 130} />
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={5} style={{ textAlign: 'center', fontSize: '14px' }}>
+            Loading more druids...
+          </td>
+        </tr>
+      </>
+    ),
+    [isMobileScreen]
+  )
 
   return (
     <>
@@ -183,24 +158,37 @@ export const Table: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              {data.map((row, index) => {
-                const podiumClassname = classNames({
-                  gold: index === 0,
-                  silver: index === 1,
-                  bronze: index === 2
-                })
-                return (
-                  <tr className={podiumClassname} key={index}>
-                    {columns.map((column, index) => {
-                      const { renderCell } = column
-                      return <td key={index}>{renderCell(row)}</td>
-                    })}
-                    {index <= 2 && (
-                      <td className={`okp4-nemeton-web-table-podium-logo ${podiumClassname}`} />
-                    )}
-                  </tr>
-                )
-              })}
+              {loading && !loadingMore && loadingRows}
+              {!loading && data.length > 0 && (
+                <React.Fragment>
+                  {data.map((row, index) => {
+                    const podiumClassname = classNames({
+                      gold: row.rank === 1,
+                      silver: row.rank === 2,
+                      bronze: row.rank === 3
+                    })
+                    return (
+                      <tr className={podiumClassname} key={index}>
+                        {columns.map((column, index) => {
+                          const { renderCell } = column
+                          return <td key={index}>{renderCell(row)}</td>
+                        })}
+                        {!isMobileScreen && index <= 2 && (
+                          <td className={`okp4-nemeton-web-table-podium-logo ${podiumClassname}`} />
+                        )}
+                      </tr>
+                    )
+                  })}
+                  {loadingMore && fetchMoreLoader}
+                </React.Fragment>
+              )}
+              {!data.length && !loading && (
+                <tr>
+                  <td colSpan={5} style={{ textAlign: 'center' }}>
+                    No druids found...
+                  </td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
