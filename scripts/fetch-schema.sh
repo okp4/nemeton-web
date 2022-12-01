da5ddb23777cdcb1dc12c51dfbfd413bf48cdbc6
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-12-01 11:58:25 +0100

    chore: bump schema version

--- scripts/fetch-schema.sh
+++ scripts/fetch-schema.sh
@@ -6,5 +6,5 @@ mkdir -p schema
 
 SCHEMA_VERSION=$(cat .schema.version)
 
-echo "Fetching 'schema.graphqls' version '${SCHEMA_VERSION}' into /graphql/schema.graphqls"
+echo "Fetching 'schema.graphqls' version '${SCHEMA_VERSION}' into /schema/schema.graphqls"
 curl -sL -H "Authorization: token ${GH_TOKEN}" "https://raw.githubusercontent.com/okp4/nemeton-leaderboard/${SCHEMA_VERSION}/graphql/schema.graphqls" -o schema/schema.graphqls
