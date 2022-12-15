490ab72bcdcee76c8061e999d5700af774b54e43
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-12-15 16:36:07 +0100

    feat(ui): implement styled profile component

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,77 @@
+import classNames from 'classnames'
+import React, { useCallback, useMemo } from 'react'
+import type { DruidProfile } from '../../entity/druid'
+import { Copy } from '../copy/Copy'
+
+export type ProfileProps = Readonly<{
+  druidProfile: DruidProfile
+  onValoperCopied: (valoper: string) => void
+}>
+
+type ActionButton = Readonly<{
+  label: string
+  href: string | null
+  disabled?: boolean
+}>
+
+export const Profile: React.FC<ProfileProps> = ({ druidProfile, onValoperCopied }) => {
+  const actionButtons: ActionButton[] = useMemo(
+    () => [
+      {
+        label: 'website',
+        href: druidProfile.website,
+        disabled: !druidProfile.website
+      },
+      {
+        label: 'explorer',
+        href: null,
+        disabled: true
+      },
+      {
+        label: 'twitter',
+        href: `https://twitter.com/${druidProfile.twitter}`,
+        disabled: !druidProfile.twitter
+      }
+    ],
+    [druidProfile.twitter, druidProfile.website]
+  )
+
+  const openInNewTab = useCallback(
+    (url: string | null) => () => {
+      if (url) {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+      }
+    },
+    []
+  )
+
+  return (
+    <div className="okp4-nemeton-web-profile-main">
+      <div
+        className="okp4-nemeton-web-profile-avatar"
+        style={{ backgroundImage: `url(${druidProfile.identity.avatar})` }}
+      />
+      <div className="okp4-nemeton-web-profile-identity-container">
+        <h2>{druidProfile.identity.name}</h2>
+        <div className="okp4-nemeton-web-profile-identity-address-wrapper">
+          <p>Address:</p>
+          <p>{druidProfile.valoper}</p>
+          <Copy item={druidProfile.valoper} onCopied={onValoperCopied} />
+        </div>
+      </div>
+      <div className="okp4-nemeton-web-profile-actions-container">
+        {actionButtons.map(({ disabled, label, href }, index) => (
+          <div
+            className={classNames(`okp4-nemeton-web-profile-action-button ${label}`, {
+              disabled
+            })}
+            key={index}
+            onClick={openInNewTab(href)}
+            title={href ?? undefined}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
