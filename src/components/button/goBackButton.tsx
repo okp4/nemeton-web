0ec31be86ffbd88c7e6a03f222424c2dfd170dc4
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-12-15 16:33:06 +0100

    feat(ui): implement goBackButton

---
+++ src/components/button/goBackButton.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+import type { NextRouter } from 'next/router'
+import { useCallback } from 'react'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+
+export const GoBackButton = (): JSX.Element => {
+  const router: NextRouter = useRouter()
+
+  const handleGoBack = useCallback((): void => {
+    router.back()
+  }, [router])
+
+  return (
+    <div className="okp4-nemeton-web-go-back-button-container" onClick={handleGoBack}>
+      <ArrowBackIcon />
+      <span>back</span>
+    </div>
+  )
+}
