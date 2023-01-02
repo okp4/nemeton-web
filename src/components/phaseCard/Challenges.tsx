4b50195b9c63a45959155df6d2d24541dc6dbd05
ErikssonJoakim <j.eriksson@okp4.com>
2023-01-02 16:44:41 +0100

    refactor: improve challenge component

--- src/components/phaseCard/Challenges.tsx
+++ src/components/phaseCard/Challenges.tsx
@@ -3,23 +3,25 @@ import LoupeIcon from '@mui/icons-material/Loupe'
 import moment from 'moment'
 import Link from 'next/link'
 import React from 'react'
-import type { Challenges as TChallenges } from '../../entity/phases/types'
+import type { Duration, Task } from '../../data/phase/dto.type'
 
 type ChallengesProps = {
-  challenges: TChallenges
+  tasks: Task[]
   refObj: React.RefObject<HTMLDivElement> | null
   onClose: () => void
-  phase: string
+  phaseName: string
+  phaseDuration: Duration
 }
 
 export const Challenges: React.FC<ChallengesProps> = ({
-  challenges: { duration, taskCategories },
-  phase,
+  tasks,
+  phaseName,
   onClose,
-  refObj
+  refObj,
+  phaseDuration
 }): JSX.Element => {
-  const from = duration ? moment(duration.from).utc().format('MMM. Do') : ''
-  const to = duration ? moment(duration.to).utc().format('MMM. Do') : ''
+  const from = moment(phaseDuration.from).utc().format('MMM. Do')
+  const to = moment(phaseDuration.to).utc().format('MMM. Do')
 
   return (
     <div className="okp4-nemeton-web-challenges-container" ref={refObj}>
@@ -27,27 +29,30 @@ export const Challenges: React.FC<ChallengesProps> = ({
         <ClearIcon className="okp4-nemeton-web-challenge-clear-icon" onClick={onClose} />
         <div className="okp4-nemeton-web-challenges-duration">
           <p>
-            Duration: {duration && moment(duration.to).diff(moment(duration.from), 'weeks')} weeks
+            Duration: {moment(phaseDuration.to).diff(moment(phaseDuration.from), 'weeks')} weeks
           </p>
           <p>{`From ${from} to ${to}`}</p>
         </div>
-        <div className="okp4-nemeton-web-challenges-summary">
-          {taskCategories.map(({ subject, tasks }, index: number) => (
-            <div className="okp4-nemeton-web-challenge-subject" key={index}>
-              <h3>{subject}</h3>
-              <div className="okp4-nemeton-web-challenges-rewards">
-                {tasks.map(({ summary, points }, index: number) => (
+
+        <div className="okp4-nemeton-web-challenges-task">
+          {tasks.map(({ group: subject, taskName, points }, index: number, array) => {
+            const previous: Task | null = index > 0 ? array[index - 1] : null
+            const mustDisplaySubject = !previous || previous.group !== subject
+            return (
+              <div className="okp4-nemeton-web-challenge-subject" key={index}>
+                {mustDisplaySubject && <h3>{subject}</h3>}
+                <div className="okp4-nemeton-web-challenges-rewards">
                   <div className="okp4-nemeton-web-challenge-reward" key={index}>
-                    <p>{summary}</p>
+                    <p>{taskName}</p>
                     <p>{points.toLocaleString()} Pts</p>
                   </div>
-                ))}
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          })}
           <div className="okp4-nemeton-web-challenge-details-container">
             <LoupeIcon />
-            <Link href={`/tasks#${phase}`}>See detailled tasks</Link>
+            <Link href={`/tasks#${phaseName}`}>See detailled tasks</Link>
           </div>
         </div>
       </div>
