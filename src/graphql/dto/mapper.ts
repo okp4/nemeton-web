a3d73b937237e3503fbad70d852eb617bcddce3d
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-01-04 16:26:49 +0100

    feat(graphql): add mapper from validator query dto to druid entity

--- src/graphql/dto/mapper.ts
+++ src/graphql/dto/mapper.ts
@@ -1,4 +1,5 @@
-import type { DruidDescriptor, PodiumDruid } from '../../entity/druid'
+import type { Druid, DruidDescriptor, PodiumDruid } from '../../entity/druid'
+import type { QValidatorQuery } from '../generated/query/types'
 import type { PodiumValidatorEdgeDTO, ValidatorEdgeDTO } from './dto.types'
 
 export const mapValidatorEdgeDTOToDruid = (dto: ValidatorEdgeDTO): DruidDescriptor => ({
@@ -24,3 +25,34 @@ export const mapPodiumValidatorEdgeDTOToPodiumDruid = (
     avatar: dto.node.identity?.picture?.href ?? '/image/avatar-fallback.webp'
   }
 })
+
+export const mapValidatorDTOToDruid = (dto: QValidatorQuery['validator']): Druid | null =>
+  dto
+    ? {
+        profile: {
+          identity: {
+            name: dto.moniker,
+            avatar: dto.identity?.picture?.href ?? '/image/avatar-fallback.webp'
+          },
+          valoper: dto.valoper,
+          website: dto.website ?? null,
+          twitter: dto.twitter ?? null,
+          points: dto.points
+        },
+
+        tasksPerPhase: dto.tasks.perPhase.map(perPhase => ({
+          phase: {
+            number: perPhase.phase.number,
+            started: perPhase.phase.started,
+            tasks: perPhase.tasks.map(t => ({
+              name: t.task.name,
+              completed: t.completed,
+              points: t.earnedPoints,
+              finished: t.task.finished,
+              started: t.task.started
+            })),
+            points: perPhase.points
+          }
+        }))
+      }
+    : null
