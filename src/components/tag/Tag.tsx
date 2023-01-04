eeea9426c2db43316c92b69fe485efc4090fcdc5
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-01-04 16:05:20 +0100

    feat(ui): add tag styled component

---
+++ src/components/tag/Tag.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type TagProps = Readonly<{
+  status: 'done' | 'idle' | 'progress' | 'error'
+  content: string
+}>
+
+export const Tag: React.FC<TagProps> = ({ status, content }) => (
+  <div className={`okp4-nemeton-web-tag-main ${status}`}>
+    <span className={`okp4-nemeton-web-tag-content ${status}`}>{content}</span>
+    <span className={`okp4-nemeton-web-tag-icon ${status}`} />
+  </div>
+)
