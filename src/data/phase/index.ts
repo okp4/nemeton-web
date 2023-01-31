41ec07bc0c625f6faf45e12d6bbcbb8b087d7560
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-01-31 15:34:44 +0100

    feat(data): update phases statuses with utils method

--- src/data/phase/index.ts
+++ src/data/phase/index.ts
@@ -4,5 +4,5 @@ import { imbolc } from './imbolc'
 import { sidh } from './sidh'
 import type { PhaseDTO } from './dto.type'
 
-const Phases = (taskUrls: TasksUrls): PhaseDTO[] => [sidh(taskUrls), imbolc(taskUrls), beltaine]
+const Phases = (taskUrls: TasksUrls): PhaseDTO[] => [sidh(taskUrls), imbolc(taskUrls), beltaine()]
 export default Phases
