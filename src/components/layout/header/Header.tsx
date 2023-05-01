895445089d77b9e8beb45034db0d5cf2e55d8336
houssemmimoun27 <houssem.mimoun@esprit.tn>
2023-05-01 16:21:55 +0100

    feat: add header mobile subsections

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
@@ -11,50 +11,29 @@ type MenuProps = Readonly<{
   routerPath: string | null
 }>
 
-type SubMenuProps = MenuProps & Readonly<{
-  selectedMenu: Route | null
-}>
+type SubMenuProps = MenuProps &
+  Readonly<{
+    selectedMenu: Route | null
+  }>
 
-const MobileMenu: React.FC<MenuProps> = ({ routerPath }): JSX.Element => {
-  const [menuOpen, setMenuOpen] = useState<boolean>(false)
-
-  const toggleMenu = useCallback(() => {
-    setMenuOpen(!menuOpen)
-  }, [menuOpen])
-
-  return (
-    <div
-      className={classNames('okp4-nemeton-web-header-links-mobile-container', { open: menuOpen })}
-    >
-      <div
-        className={`okp4-nemeton-web-header-links-mobile-burger-menu ${
-          menuOpen ? 'rotate-down' : 'rotate-up'
-        }`}
-      >
-        <Image
-          alt="mobile-menu"
-          height={40}
-          onClick={toggleMenu}
-          src="/icons/menu-dark.svg"
-          width={40}
-        />
-      </div>
-      <div className={classNames('okp4-nemeton-web-header-mobile-menu-links', { open: menuOpen })}>
-        {menu.map(({ name, path }: Route, index: number) => (
-          <React.Fragment key={path}>
-            {index >= 0 && <div className="okp4-nemeton-web-header-mobile-links-divider" />}
-            <Link href={path}>
-              <h2 className={classNames('link-label', { active: routerPath === path })}>{name}</h2>
-            </Link>
-            {index === menu.length - 1 && (
-              <div className="okp4-nemeton-web-header-mobile-links-divider" />
-            )}
-          </React.Fragment>
-        ))}
-      </div>
+const SubMenuMobile: React.FC<SubMenuProps> = ({ selectedMenu, routerPath }): JSX.Element => (
+  <div className="okp4-nemeton-web-header-mobile-sub-menu-links">
+    <div className="okp4-nemeton-web-header-mobile-sub-menu-content">
+      <div className="okp4-nemeton-web-header-menu-item-divider" />
+      {selectedMenu?.subMenu?.map(({ name, path }) => (
+        <Link
+          className={classNames('okp4-nemeton-web-header-mobile-sub-menu-item-name', {
+            active: routerPath?.startsWith(path)
+          })}
+          href={path}
+          key={path}
+        >
+          {name}
+        </Link>
+      ))}
     </div>
-  )
-}
+  </div>
+)
 
 const SubMenuDesktop: React.FC<SubMenuProps> = ({ selectedMenu, routerPath }) => (
   <div
@@ -70,18 +49,30 @@ const SubMenuDesktop: React.FC<SubMenuProps> = ({ selectedMenu, routerPath }) =>
   </div>
 )
 
-const DesktopMenu: React.FC<MenuProps> = ({ routerPath }): JSX.Element => {
+const Menu: React.FC<MenuProps> = ({ routerPath }): JSX.Element => {
   const [selectedMenu, setSelectedMenu] = useState<Route | null>(null)
-  
+  const isMobileScreen = useMediaType('(max-width: 580px)')
+  const [menuOpen, setMenuOpen] = useState<boolean>(false)
+  const separatorElement = useMemo(
+    (): JSX.Element => <div className="okp4-nemeton-web-header-menu-item-divider" />,
+    []
+  )
+
   useEffect(() => {
-    const route = menu.find((route: Route) => routerPath?.startsWith(route.path) && route.subMenu?.length)
+    const route = menu.find(
+      (route: Route) => routerPath?.startsWith(route.path) && route.subMenu?.length
+    )
     if (route) setSelectedMenu(route)
   }, [routerPath])
+
+  const toggleMenu = useCallback(() => {
+    setMenuOpen(!menuOpen)
+  }, [menuOpen])
 
   const handleClick = useCallback(
     (menuItem: Route) => () => {
       const isMenuItemSelected = menuItem.name === selectedMenu?.name
-      menuItem.subMenu && setSelectedMenu(isMenuItemSelected ? null : menuItem)
+      setSelectedMenu(menuItem.subMenu ? (isMenuItemSelected ? null : menuItem) : null)
     },
     [selectedMenu]
   )
@@ -94,43 +85,67 @@ const DesktopMenu: React.FC<MenuProps> = ({ routerPath }): JSX.Element => {
   return (
     <React.Fragment>
       <div className="okp4-nemeton-web-header-links-container">
-        {menu.map((menuItem: Route) => (
+        {isMobileScreen && (
           <div
-            className="okp4-nemeton-web-header-menu-item"
-            key={menuItem.name}
-            onClick={handleClick(menuItem)}
+            className={`okp4-nemeton-web-header-links-mobile-burger-menu ${
+              menuOpen ? 'rotate-down' : 'rotate-up'
+            }`}
           >
-            {menuItem.hash ? (
-              <Link
-                href={`${menuItem.path}${menuItem.hash}`}
-              >
+            <Image
+              alt="mobile-menu"
+              height={40}
+              onClick={toggleMenu}
+              src="/icons/menu-dark.svg"
+              width={40}
+            />
+          </div>
+        )}
+        <div
+          className={classNames('okp4-nemeton-web-header-menu-links', {
+            open: menuOpen
+          })}
+        >
+          {menu.map((menuItem: Route, index: number) => (
+            <div
+              className="okp4-nemeton-web-header-menu-item"
+              key={menuItem.name}
+              onClick={handleClick(menuItem)}
+            >
+              {isMobileScreen && index >= 0 && separatorElement}
+              {menuItem.hash ? (
+                <Link href={`${menuItem.path}${menuItem.hash}`}>
+                  <h2
+                    className={classNames('link-label', {
+                      active:
+                        (routerPath === menuItem.path ||
+                          routerPath?.substring(1) === menuItem.hash) &&
+                        !selectedMenu
+                    })}
+                  >
+                    {menuItem.name}
+                  </h2>
+                </Link>
+              ) : (
                 <h2
                   className={classNames('link-label', {
-                    active:
-                      (routerPath === menuItem.path ||
-                        routerPath?.substring(1) === menuItem.hash) &&
-                      !selectedMenu
+                    active: isActiveMenu(menuItem)
                   })}
                 >
                   {menuItem.name}
                 </h2>
-              </Link>
-            ) : (
-              <h2
-                className={classNames('link-label', {
-                  active: isActiveMenu(menuItem)
-                })}
-              >
-                {menuItem.name}
-              </h2>
-            )}
-          </div>
-        ))}
+              )}
+              {isMobileScreen && selectedMenu?.name === menuItem.name && menuItem.subMenu && (
+                <SubMenuMobile routerPath={routerPath} selectedMenu={selectedMenu} />
+              )}
+              {isMobileScreen &&
+                index === menu.length - 1 &&
+                selectedMenu?.name !== menuItem.name &&
+                separatorElement}
+            </div>
+          ))}
+        </div>
       </div>
-      <SubMenuDesktop
-        routerPath={routerPath}
-        selectedMenu={selectedMenu}
-      />
+      {!isMobileScreen && <SubMenuDesktop routerPath={routerPath} selectedMenu={selectedMenu} />}
     </React.Fragment>
   )
 }
@@ -138,7 +153,6 @@ const DesktopMenu: React.FC<MenuProps> = ({ routerPath }): JSX.Element => {
 export const Header: React.FC = () => {
   const [routerPath, setRouterPath] = useState<string | null>(null)
   const isLargeScreen = useMediaType('(min-width: 1441px)')
-  const isMobileScreen = useMediaType('(max-width: 580px)')
   const router = useRouter()
 
   useEffect(() => {
@@ -166,11 +180,7 @@ export const Header: React.FC = () => {
         <h1>incentivized testnet program</h1>
       </div>
       {!isLargeScreen && <div className="okp4-nemeton-web-header-divider" />}
-      {isMobileScreen ? (
-        <MobileMenu routerPath={routerPath} />
-      ) : (
-        <DesktopMenu routerPath={routerPath} />
-      )}
+      <Menu routerPath={routerPath} />
     </div>
   )
 }
