66d459be3df38e8c844c4c5bfa1d31a5b6a4572f
Frédéric Vilcot <fredericvilcot@gmail.com>
2023-05-22 09:38:36 +0200

    refactor(data): enhance samhain data challenges

--- src/data/phase/samhain.tsx
+++ src/data/phase/samhain.tsx
@@ -15,7 +15,7 @@ export const samhain = ({ samhain: { startDate, endDate } }: PhasesConfig): Phas
   tasks: [],
   challenges: [
     {
-      challengePart: 'part 1: Invoke the power of Dagda - From May, 24th',
+      group: 'part 1: Invoke the power of Dagda - From May, 24th',
       challengeDescription: (
         <p>
           In the Micro challenges phase, we&apos;ll be invoking the power of Dagda, one of the main
@@ -27,70 +27,43 @@ export const samhain = ({ samhain: { startDate, endDate } }: PhasesConfig): Phas
       ),
       challengeContent: [
         {
-          challengeId: 0,
-          challengeCategory: 'Connectors',
-          challenges: [
-            {
-              description: <p>Connect a storage service like S3</p>
-            },
-            {
-              description: <p>Connect an IDSA Connector</p>
-            },
-            {
-              description: <p>Connect an infrastructure service</p>
-            },
-            {
-              description: <p>Connect a business intelligence service</p>
-            }
-          ]
+          title: 'Connectors',
+          contentDescription: (
+            <>
+              <p>Connect a storage service like S3</p>
+              <p>Connect an IDSA Connector</p>
+              <p>Connect an infrastructure service</p>
+              <p>Connect a business intelligence service</p>
+            </>
+          )
         },
         {
-          challengeId: 1,
-          challengeCategory: 'Governance',
-          challenges: [
-            {
-              description: <p>Propose a governance template</p>
-            },
-            {
-              description: <p>Implement decentralised governance (multsigs, IBC-imported tokens)</p>
-            },
-            {
-              description: <p>Tests some rules in Prolog about access rights and business models</p>
-            }
-          ]
+          title: 'Governance',
+          contentDescription: (
+            <>
+              <p>Propose a governance template</p>
+              <p>Implement decentralised governance (multsigs, IBC-imported tokens)</p>
+              <p>Tests some rules in Prolog about access rights and business models</p>
+            </>
+          )
         },
         {
-          challengeId: 2,
-          challengeCategory: 'Tools',
-          challenges: [
-            {
-              description: <p>OKP4 protocol explorer (e.g object storage tool</p>
-            }
-          ]
+          title: 'Tools',
+          contentDescription: <p>OKP4 protocol explorer (e.g object storage tool)</p>
         },
         {
-          challengeId: 3,
-          challengeCategory: 'Security & Privacy',
-          challenges: [
-            {
-              description: <p>Connect a privacy preserving service</p>
-            },
-            {
-              description: <p>ZKP service execution</p>
-            },
-            {
-              description: <p>Homomorphic data processing</p>
-            }
-          ]
+          title: 'Security & Privacy',
+          contentDescription: (
+            <>
+              <p>Connect a privacy preserving service</p>
+              <p>ZKP service execution</p>
+              <p>Homomorphic data processing</p>
+            </>
+          )
         },
         {
-          challengeId: 4,
-          challengeCategory: 'Identity',
-          challenges: [
-            {
-              description: <p>Connect an identity provider like Keycloack</p>
-            }
-          ]
+          title: 'Identity',
+          contentDescription: <p>Connect an identity provider like Keycloack</p>
         }
       ]
     }
