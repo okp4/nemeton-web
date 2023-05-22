2bcb2f36a9003b174a620b3a782c86d19da29732
houssemmimoun27 <houssem.mimoun@esprit.tn>
2023-05-22 17:25:33 +0100

    refactor: update rewards content

--- src/pages/rewards.tsx
+++ src/pages/rewards.tsx
@@ -1,5 +1,3 @@
-import { useMemo } from 'react'
-import { useRouter } from 'next/router'
 import { Head } from '@/components/head/Head'
 import { Footer } from '@/components/layout/footer/Footer'
 import { Header } from '@/components/layout/header/Header'
@@ -10,26 +8,14 @@ import type { Config } from '@/types/config.type'
 export type RewardsProps = Pick<Config, 'title' | 'keywords' | 'description' | 'urls'>
 
 const Rewards: NextPage<RewardsProps> = props => {
-  const router = useRouter()
   const { urls } = props
 
-  const renderRewardsContent = useMemo(
-    () =>
-      router.asPath.startsWith('/builders') ? (
-        <>
-          <h1>Rewards</h1>
-          <p>
-            Awarded Builders will be rewarded with mainnet tokens. They will also have a head start
-            when looking for Grants from the OKP4 Foundation.
-          </p>
-          <p>
-            A maximum of 2% of the total supply of mainnet tokens will be allocated to Builders
-            based on their contribution judged by the OKP4 core team. OKP4 reserves the right to not
-            allocate the entirety of the rewards allocated for Builders.
-          </p>
-        </>
-      ) : (
-        <>
+  return (
+    <div className="okp4-nemeton-web-page-main">
+      <Head {...props} />
+      <main>
+        <Header />
+        <div className="okp4-nemeton-web-page-content-container" id="rewards">
           <h1>Rewards</h1>
           <p>Druids will be rewarded with a mix of token rewards + delegation commitment.</p>
           <p>
@@ -38,18 +24,6 @@ const Rewards: NextPage<RewardsProps> = props => {
             foundation vesting) will be delegated to Druids based on their participation and
             performance during the program.
           </p>
-        </>
-      ),
-    [router.asPath]
-  )
-
-  return (
-    <div className="okp4-nemeton-web-page-main">
-      <Head {...props} />
-      <main>
-        <Header />
-        <div className="okp4-nemeton-web-page-content-container" id="rewards">
-          {renderRewardsContent}
         </div>
         <Footer urls={urls} />
       </main>
